9c644a90769dd57953c8903c53c5c98afdbe999b
pooja-k-979 <87581023+pooja-k-979@users.noreply.github.com>
2021-07-23 03:07:34 +0530

    Add files via upload

---
+++ ec-project/ec_index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>My first website</title>
+        <!--minifies bootstrap-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script><meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="ec_style.css" type="text/css" rel="stylesheet"/>
+        
+    </head>
+    <body>
+<!--       navbar
+       
+-->        <nav class="nav navbar navbar-inverse ">
+            <div class="container nav_container">
+                <div class="navbar-header ">
+                       <a href="#" class="navbar-brand">E-Store</a>
+                </div>
+                      
+                <div class="collapse navbar-collapse" id="myNavbar">
+                <div>
+                         <ul class="nav navbar-nav navbar-right">
+                            <li><a href="ec_signup.php" target="_blank"><span class="glyphicon glyphicon-user"></span> Sign up</a></li>
+                            <li><a href="#modalbox" ><span class="glyphicon glyphicon-log-in"></span> Login</a> </li>
+                            <li><a href="#"><span class="glyphicon glyphicon-th-list"></span> About us</a></li>
+                            <li><a href="#"><span class=" glyphicon glyphicon-earphone"></span> Contact us</a></li>
+                         </ul>
+                             
+                </div>
+            </div>
+         </nav>
+
+      <!--body--mobile purchasing-->
+      
+      <div class="index_back">
+         
+          <div class="container ">
+           <!--1st row-->
+           <div class="row row_style">
+               <!--1st mobile-->
+               <div class="col-xs-4">
+                   <div class="panel panel-default">
+                       <div class="panel-heading">
+                           <h4> Redmi 9 prime</h4>
+                       </div>
+                       <div class="panel-body">
+                           <img class="size" href="#" src="ecommerce images/redmi_9_prime.jpg" alt="weak internet connection">
+                           <p>--Redmi 9 Prime (Mint Green, 4GB RAM, 64GB Storage)- Full HD+ Display & AI Quad Camera
+Style name:4GB RAM 64GB Storage
+Colour:Mint Green</p> 
+                           <button class="btn btn-primary btn_size">Order now</button>
+                       </div>
+                   </div>
+               </div>
+               <!--2nd mobile-->
+               <div class="col-xs-4">
+                  <div class="panel panel-default">
+                       <div class="panel-heading">
+                           <h4> Redmi 9 prime</h4>
+                       </div>
+                       <div class="panel-body">
+                           <img class="size" href="#" src="ecommerce images/redmi_9_prime.jpg" alt="weak internet connection">
+                           <p>--Redmi 9 Prime (Mint Green, 4GB RAM, 64GB Storage)- Full HD+ Display & AI Quad Camera
+Style name:4GB RAM 64GB Storage
+Colour:Mint Green</p> 
+                           <button class="btn btn-primary btn_size btn_size">Order now</button>
+                       </div>
+                   </div>  
+               </div>
+               <!--3rd element-->
+               <div class="col-xs-4">
+                  <div class="panel panel-default">
+                       <div class="panel-heading">
+                           <h4> Redmi 9 prime</h4>
+                       </div>
+                       <div class="panel-body">
+                           <img class="size" href="#" src="ecommerce images/redmi_9_prime.jpg" alt="weak internet connection">
+                           <p>--Redmi 9 Prime (Mint Green, 4GB RAM, 64GB Storage)- Full HD+ Display & AI Quad Camera
+Style name:4GB RAM 64GB Storage
+Colour:Mint Green</p> 
+                           <button class="btn btn-primary btn_size">Order now</button>
+                       </div>
+                   </div>  
+               </div>
+              
+               
+           </div>
+           <!--2nd row-->
+           <div class="row row_style">
+               <!--1st mobile-->
+               <div class="col-xs-4">
+                  <div class="panel panel-default">
+                       <div class="panel-heading">
+                           <h4> Redmi 9 prime</h4>
+                       </div>
+                       <div class="panel-body">
+                           <img class="size" href="#" src="ecommerce images/redmi_9_prime.jpg" alt="weak internet connection">
+                           <p>--Redmi 9 Prime (Mint Green, 4GB RAM, 64GB Storage)- Full HD+ Display & AI Quad Camera
+Style name:4GB RAM 64GB Storage
+Colour:Mint Green</p> 
+                           <button class="btn btn-primary btn_size">Order now</button>
+                       </div>
+                   </div>  
+               </div>
+               <!--2nd mobile-->
+               <div class="col-xs-4">
+                  <div class="panel panel-default">
+                       <div class="panel-heading">
+                           <h4> Redmi 9 prime</h4>
+                       </div>
+                       <div class="panel-body">
+                           <img class="size" href="#" src="ecommerce images/redmi_9_prime.jpg" alt="weak internet connection">
+                           <p>--Redmi 9 Prime (Mint Green, 4GB RAM, 64GB Storage)- Full HD+ Display & AI Quad Camera
+Style name:4GB RAM 64GB Storage
+Colour:Mint Green</p> 
+                           <button class="btn btn-primary btn_size">Order now</button>
+                       </div>
+                   </div>  
+               </div>
+               <!--3rd element-->
+               <div class="col-xs-4">
+                  <div class="panel panel-default">
+                       <div class="panel-heading">
+                           <h4> Redmi 9 prime</h4>
+                       </div>
+                       <div class="panel-body">
+                           <img class="size" href="#" src="ecommerce images/redmi_9_prime.jpg" alt="weak internet connection">
+                           <p>--Redmi 9 Prime (Mint Green, 4GB RAM, 64GB Storage)- Full HD+ Display & AI Quad Camera
+Style name:4GB RAM 64GB Storage
+Colour:Mint Green</p> 
+                           <button class="btn btn-primary btn_size">Order now</button>
+                       </div>
+                   </div>  
+               </div>
+               <br><br>
+           </div>
+           
+       </div>
+          
+       </div>
+      
+       <!--footer-->
+       <div class="footer footer_style">
+           <div class="row ">
+               
+               <div class="col-xs-4 ">
+                  <div class="panel panel-default column_style">
+                      <h4>Information</h4>
+                   </div>
+                   </div>
+               <div class="col-xs-4 ">
+                   <div class="panel panel-default column_style">
+                      <h4>My account</h4>
+                   </div>
+               </div>
+               <div class="col-xs-4 ">
+                   <div class="panel panel-default column_style">
+                      <h4>Contact us</h4>
+                   </div>
+               </div>
+               
+               <div class="col-xs-4">
+                   <div class="panel panel-default column_style">
+                      <h6>About us</h6>
+                   </div>
+               </div>
+               
+               <div class="col-xs-4">
+                   <div class="panel panel-default column_style">
+                       <a href="ec_signin.php"><h6><h6>Login</h6></a>
+                      
+                   </div>
+               </div>
+               
+               <div class="col-xs-4">
+                   <div class="panel panel-default column_style">
+                      <h6>Contact +91-123-0000000</h6>
+                   </div>
+               </div>
+               
+               <div class="col-xs-4">
+                   <div class="panel panel-default column_style">
+                      <h6>Contact us</h6>
+                   </div>
+               </div>
+               
+               <div class="col-xs-4">
+                   <div class="panel panel-default column_style" >
+                       <a href="ec_signup.php"><h6>Sign up</h6></a>
+                   </div>
+               </div>
+                   
+               
+           </div>
+       </div>
+       
+      
+    
+                            <div id="modalbox" class="modal">
+                                <div class="modalcontent">
+                                
+                                    <h4>Don't have an account?<a href="ec_signup.php">Sign up</a></h4>
+                                    <br>
+                                <form id="Register_modal" method="post" action="user_registration_script.php">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="email" placeholder="Email" >
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control" name="password" placeholder="Password" >
+                                    </div>
+                                
+                                     <div class="row">
+                                         <div class="col-lg-4">
+                                             
+                                         </div>
+                                         <div class="col-lg-4">
+                                             <button <input type="submit" class="btn btn-primary btn-block">Continue</button>
+                                         </div>
+                                         <div class="col-lg-4">
+                                             <div class="callout" data-closable>
+                                                <button class="close-button modalclose" aria-label="Close alert" type="button" data-close>
+                                                    <a href="#" class="modalclose">Close</a>
+                                                </button>
+                                             <!--<a href="#" class="modalclose">Close</a>-->
+                                             <!--<button <input type="close" class="btn btn-primary btn-block ">Close</button>-->
+                                             </div>
+                                         </div>
+                                     </div>
+                                 
+                                 </form>
+                                    
+                                    
+                                    <a href="#" class="modalclose">&times;</a>
+                                </div>
+                            </div>   
+       
+    </body>
+</html>
---
+++ ec-project/ec_signup.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    
+    <head>
+        <title>Sign up</title>
+        <!--minifies bootstrap-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+<!--        <style>
+         
+               .modal_container{
+  background-color: rgba(0,0,0,0.3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  border-width: 20px;
+ height: 100%;
+  width: 100%;
+}
+.modal{
+    background-color: #fff;
+    border-width: 20px;
+    border-radius: 20px;
+    border-color: #c7254e;
+    box-shadow: 0 2px 4px rgba(0,0,0,0.7);
+    padding: 30px 50px;
+    width: 300px;
+    max-width: 100%; /*keep the modal content inside the modal*/
+    text-align: center;
+        </style>-->
+        
+        <link href="ec_style.css" type="text/css" rel="stylesheet"/>
+        
+    </head>
+    
+<!--    <head>
+        <title>Sign up</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="estyle.css" type="text/css" rel="stylesheet"/>
+        <style>
+         
+               .modal_container{
+  background-color: rgba(0,0,0,0.3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  border-width: 20px;
+ height: 100%;
+  width: 100%;
+}
+.modal{
+    background-color: #fff;
+    border-width: 20px;
+    border-radius: 20px;
+    border-color: #c7254e;
+    box-shadow: 0 2px 4px rgba(0,0,0,0.7);
+    padding: 30px 50px;
+    width: 300px;
+    max-width: 100%; /*keep the modal content inside the modal*/
+    text-align: center;
+        </style>
+     
+    </head>-->
+    <body>
+        
+        
+        <div class="hero">
+             
+               <div class="modal-container"  id="modal_container">
+                       
+                             <div id="modal" >
+                                 <form id="Register_modal" method="post" action="user_registration_script.php">
+                                
+                                     <div class="form-group">
+                                        <!--<label for="yourname">Your name:</label>-->
+                                        <input type="text" class="form-control input-lg" name="yourname" placeholder="Name">   
+                                     </div>
+                                     <div class="form-group">
+                                        <input type="text" class="form-control" name="email" placeholder="Email" >
+                                     </div>
+                                     <div class="form-group">
+                                        <input type="password" class="form-control" name="email" placeholder="Password" >
+                                    </div>
+                                    <div class="form-group">
+                                         <input type="tel" class="form-control" name="contact" placeholder="Contact" >  
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="city" placeholder="City" >  
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="address" placeholder="Address" >  
+                                    </div>
+                                
+                                     <div class="row">
+                                         <div class="col-lg-4">
+                                             
+                                         </div>
+                                         <div class="col-lg-4">
+                                             <button <input type="submit" class="btn btn-primary btn-block">Continue</button>
+                                         </div>
+                                         <div class="col-lg-4">
+                                             <button <input type="close" class="btn btn-primary btn-block">Close</button>
+                                         </div>
+                                     </div>
+                                 
+                                 </form>
+                             </div>
+                </div> 
+            <div class="bubbles">
+                <img src="ecommerce images/bubble.png"> 
+                <img src="ecommerce images/bubble.png">
+                <img src="ecommerce images/bubble.png">
+                <img src="ecommerce images/bubble.png">
+                <img src="ecommerce images/bubble.png">
+                <img src="ecommerce images/bubble.png">
+                <img src="ecommerce images/bubble.png">
+                <img src="ecommerce images/bubble.png">
+                <img src="ecommerce images/bubble.png">
+                <img src="ecommerce images/bubble.png">
+            </div>
+                        
+    </div>
+        
+</body>
+</html>
